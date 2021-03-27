65c13dde478767c53b30dc09856cc1e505a878cc
dimas-reza <67844456+dimas-reza@users.noreply.github.com>
2021-03-27 20:09:12 +0700

    Add files via upload

---
+++ Temparature_Convert.py
@@ -0,0 +1,13 @@
+print("")
+print("=====MENGHITUNG TEMPERATUR=====")
+print(" kelvin to fahrenheit")
+
+
+
+Kelvin = float(input("Berapa nilai suhu = "))
+print("Suhu ", Kelvin, "Kelvin setara dengan ")
+print("")
+
+#Menghitung fahrenheit
+fahrenheit = (9/5) * (Kelvin - 273) + 32
+print(fahrenheit, " fahrenheit")
